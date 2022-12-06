5ed0c136142e986b96b514b6c02dfb29c75ddcd7
기석 김 <rltjr9604@pukyong.ac.kr>
2022-12-07 01:50:59 +0900

    Complete Problem 42.

--- Problem 42/Problem 42.py
+++ Problem 42/Problem 42.py
@@ -1,1 +1,23 @@
-print("1")
+import numpy as np
+
+def istriangle_number(n):
+    estimate = (-1+np.sqrt(1+8*n))/2
+    if abs(int(estimate) - estimate) < 1e-6:
+        return True
+    else:
+        return False
+
+def run():
+    Total_Numbers = 0
+    for word in words:
+        number = 0
+        for alphabet in word:
+            if alphabet != '"':
+                number += ord(alphabet) - 64
+        if istriangle_number(number):
+            Total_Numbers += 1
+    print("Total number of Triangles words : {0}".format(Total_Numbers))
+
+words = np.loadtxt("Problem 42\p042_words.txt",dtype = str,delimiter=',')
+run()
+
